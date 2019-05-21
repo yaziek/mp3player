7856f275686b366f2baa56e40ceb494930a96b71
yaziek <yaziek@gmail.com>
2019-05-21 21:42:49 +0200

    controllers divided in diff classess and fxml's

---
+++ src/main/java/pl/javastart/mp3player/controller/ContentPaneController.java
@@ -0,0 +1,15 @@
+package pl.javastart.mp3player.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class ContentPaneController {
+
+    @FXML
+    private TableView<?> contentTable;
+
+    public void initialize(){
+        System.out.println("Content controller created");
+    }
+
+}
--- src/main/java/pl/javastart/mp3player/controller/MainController.java
+++ src/main/java/pl/javastart/mp3player/controller/MainController.java
@@ -1,67 +1,8 @@
 package pl.javastart.mp3player.controller;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.input.MouseEvent;
-
 public class MainController {
 
-    @FXML
-    private MenuItem fileMenuItem;
-
-    @FXML
-    private MenuItem dirMenuItem;
-
-    @FXML
-    private MenuItem closeMenuItem;
-
-    @FXML
-    private MenuItem aboutMenuItem;
-
-    @FXML
-    private TableView<?> contentTable;
-
-    @FXML
-    private Button previousButton;
-
-    @FXML
-    private ToggleButton playButton;
-
-    @FXML
-    private Button nextButton;
-
-    @FXML
-    private Slider volumeSlider;
-
-    @FXML
-    private Slider progressSlider;
-
     public void initialize() {
-        configureButtons();
-        configureVolume();
-    }
-
-    private void configureVolume() {
-        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->
-        System.out.println("Wciśnięto przycisk na suwaku głośności")
-        );
-    }
-
-    private void configureButtons() {
-        previousButton.setOnAction(actionEvent -> System.out.println("Poprzednia piosenka"));
-        nextButton.setOnAction(actionEvent -> System.out.println("Poprzednia piosenka"));
-        playButton.setOnAction(actionEvent -> {
-            if (playButton.isSelected()) {
-                System.out.println("Play");
-            } else {
-                System.out.println("Stop");
-            }
-        });
+        System.out.println("Main controller created");
     }
 }
