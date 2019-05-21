fb83cae1e5e154965cdb44adf1fb9f0e4ff429d9
yaziek <yaziek@gmail.com>
2019-05-21 21:12:21 +0200

    added events handlers

--- src/main/java/pl/javastart/mp3player/controller/MainController.java
+++ src/main/java/pl/javastart/mp3player/controller/MainController.java
@@ -1,11 +1,14 @@
 package pl.javastart.mp3player.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
@@ -40,7 +43,25 @@ public class MainController {
     private Slider progressSlider;
 
     public void initialize() {
-        System.out.println("MainController created");
+        configureButtons();
+        configureVolume();
     }
 
+    private void configureVolume() {
+        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->
+        System.out.println("Wciśnięto przycisk na suwaku głośności")
+        );
+    }
+
+    private void configureButtons() {
+        previousButton.setOnAction(actionEvent -> System.out.println("Poprzednia piosenka"));
+        nextButton.setOnAction(actionEvent -> System.out.println("Poprzednia piosenka"));
+        playButton.setOnAction(actionEvent -> {
+            if (playButton.isSelected()) {
+                System.out.println("Play");
+            } else {
+                System.out.println("Stop");
+            }
+        });
+    }
 }
